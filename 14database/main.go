f06667b42cc534943215188d7c6aecbf561431fd
lxn <1933650668@qq.com>
2025-05-10 13:13:36 +0800

    添加更新、删除和插入用户的功能，完善数据库操作示例

--- 14database/main.go
+++ 14database/main.go
@@ -9,8 +9,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
-
 type auth_user struct {
 	id        int
 	username  string
@@ -52,6 +50,32 @@ func getMany(db *sql.DB, id int) ([]auth_user, error) {
 	return users, nil
 }
 
+// update
+func updata(db *sql.DB, user auth_user) error {
+	query := "UPDATE auth_user SET username = ?, email = ?, is_active = ? WHERE id = ?"
+	_, err := db.Exec(query, user.username, user.email, user.is_active, user.id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// delete
+func delete(db *sql.DB, id int) error {
+	query := "DELETE FROM auth_user WHERE id = ?"
+	_, err := db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func insert(db *sql.DB, user auth_user) (result sql.Result, err error) {
+	query := "INSERT INTO auth_user (password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	result, err = db.Exec(query, "QWER1234", 1, user.username, "LU", "XUNAN", user.email, 1, user.is_active, "2023-10-01 12:00:00")
+	return
+}
+
 func main() {
 
 	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, server, port, database)
@@ -81,4 +105,32 @@ func main() {
 	}
 	fmt.Println(user)
 
+	//update
+	// user.email = "1933650668@qq.com"
+	// user.is_active = false
+	// err_update := updata(db, user)
+
+	// if err_update != nil {
+	// 	log.Fatalln(err_update)
+	// }
+	// fmt.Println("Update successful!")
+	// fmt.Println(getOne(db, 1))
+	// fmt.Println(getMany(db, 1))
+
+	// delete(db, 2)
+	// fmt.Println(getMany(db, 1))
+
+	//insert
+	user.username = "LUXUNAN"
+	user.email = "1234@qq.com"
+	user.is_active = true
+	result_insert, err_insert := insert(db, user)
+	if err_insert != nil {
+		log.Fatalln(err_insert)
+	}
+	fmt.Println("Insert successful!")
+	id, _ := result_insert.LastInsertId()
+
+	fmt.Println("Inserted ID:", id)
+	fmt.Println(getOne(db, int(id)))
 }
