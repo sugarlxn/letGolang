4b551874d310245580b4ffe98591da9017752c40
lxn <1933650668@qq.com>
2025-05-17 19:16:23 +0800

    添加 web_tutorial 模块，包括 HTML 页面、HTTP 服务器和测试用例

--- web_tutorial/main.go
+++ web_tutorial/main.go
@@ -1,14 +1,58 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type myhander struct{}
+
+func (h *myhander) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
+
+type aboutHandler struct{}
+
+func (h *aboutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("about page"))
+}
 
 func main() {
 
+	mh := myhander{}
+	ah := aboutHandler{}
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: nil,
 	}
+	http.Handle("/hello", &mh)
+	http.Handle("/about", &ah)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("# root page"))
+	},
+	)
+
+	//post test
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		length := r.ContentLength
+		body := make([]byte, length)
+		r.Body.Read(body)
+		w.Write([]byte("post body: " + string(body)))
+	})
+
+	http.Handle("/aaa", http.NotFoundHandler())
+	http.Handle("/bbb", http.RedirectHandler("http://www.bing.com", 302))
+
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		fmt.Fprintln(w, "name:", r.FormValue("name"))
+		fmt.Fprintln(w, r.Form)
+	})
+
 	server.ListenAndServe()
 	// http.ListenAndServe("localhost:8080", nil)
+	// http.Handle()
+	// http.DefaultServeMux
+	// http.HandleFunc()
 
 }
